5280ba78c5808e4997b3eaf5a0776e5a7d462db5
Tati07f <ta_jakovleva@mail.ru>
2022-09-25 10:52:42 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <p className={styles.description}>
           Now are going to start new project: 
           e-app
-          9/25/2022
+          
         </p>
         <p className={styles.description}>
          
@@ -32,16 +32,8 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        
+        2022
       </footer>
     </div>
   )
