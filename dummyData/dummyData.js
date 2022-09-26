e0691dbc01777672fcf2a661c4e16784913377be
nozky <norvillie.villaruel@edu.sait.ca>
2022-09-26 17:42:19 -0600

    fix data

--- dummyData/dummyData.js
+++ dummyData/dummyData.js
@@ -1,11 +1,13 @@
 
 // If you think we still need more property, just add them here.
-export const dummyData = {
-  productId: "",
-  productName: "",
-  productDescription: "",
-  productImages: [],
-  productPrice: 0.00,
-  productSlug: ""
-}
+export const dummyData = [
+  {
+    productId: "",
+    productName: "",
+    productDescription: "",
+    productImages: [],
+    productPrice: 0.00,
+    productSlug: ""
+  }
+]
 
